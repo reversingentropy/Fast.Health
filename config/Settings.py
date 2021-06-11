a9d689f7f187b63416011517691507adecaf6991
Daniel Wong <suchunw@hotmail.com>
2021-06-11 10:38:18 +0800

    danwsc trying to add Settings.py

---
+++ config/Settings.py
@@ -0,0 +1,22 @@
+import os
+
+class Settings:
+
+    secretKey="a12nc)238OmPq#cxOlm*a" # your own secret key
+
+    modelFile = "data/model.pkl" # machine learning model
+
+    dbUsed = 'maria'
+
+    if dbUsed == 'maria':
+        #Staging on local machine
+        host='localhost'
+        database=''
+        user=''
+        password=''
+    else:
+        #Staging on heroku
+        host=os.environ['HOST']
+        database=os.environ['DATABASE']
+        user=os.environ['USERNAME']
+        password=os.environ['PASSWORD']
