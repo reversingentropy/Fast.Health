9a727271140d29da5792f36d96abd8a01ab32fc4
sumcp1 <sum_chee_ping01.pt@ichat.sp.edu.sg>
2021-06-17 15:43:43 +0800

    setting

--- config/Settings.py
+++ config/Settings.py
@@ -6,14 +6,14 @@ class Settings:
 
     modelFile = "data/model.pkl" # machine learning model
 
-    dbUsed = 'pooling'
+    dbUsed = 'local'
 
-    if dbUsed == 'pooling':
+    if dbUsed == 'local':
         #Staging on local machine
-        host=''
-        database=''
-        user=''
-        password=''
+        host='localhost'
+        database='fasthealth'
+        user='root'
+        password='sum381Tk!'
 
     else:
         #Staging on heroku
